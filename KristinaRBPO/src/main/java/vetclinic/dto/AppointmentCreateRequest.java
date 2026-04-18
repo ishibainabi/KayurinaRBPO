47cd789a412f441217a966333d6a7b5da706c2e3
Кристина Каюрина <ishibainabi@gmail.com>
2026-04-18 16:45:24 +0300

    Reformat getters and setters in AppointmentCreateRequest

--- KristinaRBPO/src/main/java/vetclinic/dto/AppointmentCreateRequest.java
+++ KristinaRBPO/src/main/java/vetclinic/dto/AppointmentCreateRequest.java
@@ -3,16 +3,32 @@ package vetclinic.dto;
 import java.time.LocalDateTime;
 
 public class AppointmentCreateRequest {
+
     private Long petId;
     private Long vetId;
     private LocalDateTime appointmentTime;
 
-    public Long getPetId() { return petId; }
-    public void setPetId(Long petId) { this.petId = petId; }
+    public Long getPetId() {
+        return petId;
+    }
 
-    public Long getVetId() { return vetId; }
-    public void setVetId(Long vetId) { this.vetId = vetId; }
+    public void setPetId(Long petId) {
+        this.petId = petId;
+    }
 
-    public LocalDateTime getAppointmentTime() { return appointmentTime; }
-    public void setAppointmentTime(LocalDateTime appointmentTime) { this.appointmentTime = appointmentTime; }
+    public Long getVetId() {
+        return vetId;
+    }
+
+    public void setVetId(Long vetId) {
+        this.vetId = vetId;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
 }
