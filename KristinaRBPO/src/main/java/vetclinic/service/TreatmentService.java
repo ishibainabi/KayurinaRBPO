3cbce5c0e3bf95fbb072a43abe4aae97fd901074
Кристина Каюрина <ishibainabi@gmail.com>
2026-04-18 15:54:19 +0300

    Refactor TreatmentService to manage appointments
    Refactor TreatmentService to include AppointmentRepository for managing appointments associated with treatments. Update save and update methods to ensure appointments are validated before saving treatments.

--- KristinaRBPO/src/main/java/vetclinic/service/TreatmentService.java
+++ KristinaRBPO/src/main/java/vetclinic/service/TreatmentService.java
@@ -1,17 +1,23 @@
 package vetclinic.service;
 
 import org.springframework.stereotype.Service;
+import vetclinic.model.entity.Appointment;
 import vetclinic.model.entity.Treatment;
+import vetclinic.repository.AppointmentRepository;
 import vetclinic.repository.TreatmentRepository;
 
 import java.util.List;
 
 @Service
 public class TreatmentService {
-    private final TreatmentRepository treatmentRepository;
 
-    public TreatmentService(TreatmentRepository treatmentRepository) {
+    private final TreatmentRepository treatmentRepository;
+    private final AppointmentRepository appointmentRepository;
+
+    public TreatmentService(TreatmentRepository treatmentRepository,
+                            AppointmentRepository appointmentRepository) {
         this.treatmentRepository = treatmentRepository;
+        this.appointmentRepository = appointmentRepository;
     }
 
     public List<Treatment> findAll() {
@@ -19,11 +25,19 @@ public class TreatmentService {
     }
 
     public Treatment findById(Long id) {
-        return treatmentRepository.findById(id).orElse(null);
+        return treatmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Treatment not found"));
     }
 
-    public Treatment save(Treatment treatment) {
-        return treatmentRepository.save(treatment);
+    public Treatment save(Treatment t) {
+
+        Appointment appointment = appointmentRepository.findById(
+                t.getAppointment().getId()
+        ).orElseThrow(() -> new RuntimeException("Appointment not found"));
+
+        t.setAppointment(appointment);
+
+        return treatmentRepository.save(t);
     }
 
     public Treatment update(Long id, Treatment data) {
@@ -31,7 +45,14 @@ public class TreatmentService {
                 .orElseThrow(() -> new RuntimeException("Treatment not found"));
 
         existing.setDescription(data.getDescription());
-        existing.setAppointment(data.getAppointment());
+
+        if (data.getAppointment() != null) {
+            Appointment appointment = appointmentRepository.findById(
+                    data.getAppointment().getId()
+            ).orElseThrow(() -> new RuntimeException("Appointment not found"));
+
+            existing.setAppointment(appointment);
+        }
 
         return treatmentRepository.save(existing);
     }
