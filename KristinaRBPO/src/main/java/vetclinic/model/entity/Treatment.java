51f7410861e71abd6363072237449bdf7bd6e26d
Кристина Каюрина <ishibainabi@gmail.com>
2026-04-18 17:10:14 +0300

    Change appointment relationship from OneToOne to ManyToOne

--- KristinaRBPO/src/main/java/vetclinic/model/entity/Treatment.java
+++ KristinaRBPO/src/main/java/vetclinic/model/entity/Treatment.java
@@ -15,21 +15,39 @@ public class Treatment {
     @Column(nullable = false)
     private String description;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "appointment_id", nullable = false)
     @JsonIgnore
     private Appointment appointment;
 
     public Treatment() {}
+
     public Treatment(String description, Appointment appointment) {
         this.description = description;
         this.appointment = appointment;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public Appointment getAppointment() { return appointment; }
-    public void setAppointment(Appointment appointment) { this.appointment = appointment; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
 }
