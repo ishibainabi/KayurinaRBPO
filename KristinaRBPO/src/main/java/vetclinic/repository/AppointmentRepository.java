c92cb5114665614704384b9d742e3581a1c46b37
Кристина Каюрина <ishibainabi@gmail.com>
2026-05-02 16:03:20 +0300

    Add methods to check vet and pet availability

--- KristinaRBPO/src/main/java/vetclinic/repository/AppointmentRepository.java
+++ KristinaRBPO/src/main/java/vetclinic/repository/AppointmentRepository.java
@@ -3,5 +3,13 @@ package vetclinic.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vetclinic.model.entity.Appointment;
 
+import java.time.LocalDateTime;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    // Проверка занятости врача
+    boolean existsByVetIdAndAppointmentTime(Long vetId, LocalDateTime appointmentTime);
+
+    // (опционально) Проверка занятости питомца
+    boolean existsByPetIdAndAppointmentTime(Long petId, LocalDateTime appointmentTime);
 }
